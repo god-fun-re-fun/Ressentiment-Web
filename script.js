b78fa92088b53ffca9953d7e1caa1161bef117bb
LeeJoEun-01 <joeun1005@naver.com>
2024-04-23 12:13:37 +0900

    Ressentiment 웹사이트 제작 시작
    html, css, js 파일 만들기

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('infoBtn').addEventListener('click', function() {
+  alert('Info 버튼이 클릭되었습니다.');
+});
+
+document.getElementById('aboutBtn').addEventListener('click', function() {
+  alert('About 버튼이 클릭되었습니다.');
+});
